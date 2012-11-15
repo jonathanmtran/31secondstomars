d6b6696f32a5e1ee0b7fd0de4302f3c645f6290c
Jonathan Tran <jtran@cloud.jonathanmtran.com>
2012-11-14 18:41:55 -0800

    Added Twitter feed of @nasa to the sidebar

--- sidebar.php
+++ sidebar.php
@@ -7,5 +7,10 @@
 					<a href="https://twitter.com/nasa"><img src="images/twitter.png" alt="follow NASA on twitter" style="padding: 0px; border: none; background: transparent;"/></a>
 				</p>
 			</div>
-	
+			
+			<div class="sidebox">
+				<a class="twitter-timeline"  href="https://twitter.com/NASA" data-widget-id="268906414771146753">Tweets by @NASA</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+			</div>
+			
 		</div>
