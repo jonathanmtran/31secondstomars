a08c47f5c43dd700d39d339d44db6a0e7c6788fb
Jonathan M Tran <jonathanmtran@about.me>
2012-11-27 15:30:30 -0800

    Changed first level headers to second level

--- information.php
+++ information.php
@@ -7,37 +7,37 @@ include 'header.php';
 		
 			<a name="TemplateInfo"></a>
 			<h1>Information</h1>
-			<h1>Interesting Facts</h1>
+			<h2>Interesting Facts</h2>
 			<p>Mars is named after the ancient Roman god of war, as befitting the red planet's bloody color. The Romans copied the ancient Greeks, who named the fourth planet from the sun after their god of war, Ares.</p>
-
-			<h1>Planet mass, diameter and orbit:</h1>
+			
+			<h2>Planet mass, diameter and orbit:</h2>
 			<ul>
 				<li>Orbit:227,940,00 km from the sun</li>
 				<li>Diameter:6,794 km</li>
 				<li>Mass:6.421e23 kg</li>
 			</ul>
-
-			<h1>Temperature</h1>
+			
+			<h2>Temperature</h2>
 			<ul>
 				<li>Average Temperature -55 C or -67 F</li>
 				<li>Winter: -133 C to -207 F</li>
 				<li>Summer: 27C or 80 F</li>
 			</ul>
-
-			<h1>Exploration</h1>
+			
+			<h2>Exploration</h2>
 			<ul>
 				<li>The first spacecraft to visit Mars was Mariner 4 in 1965.</li>
-				<li>Mars 2 was the first spacecraft to land actually land on mars in 1976.<li>
+				<li>Mars 2 was the first spacecraft to land actually land on mars in 1976.</li>
 			</ul>
-
-			<h1>Geography</h1>
+			
+			<h2>Geography</h2>
 			<ul>
 				<li>The bright rust color Mars is known for is due to to iron-rich minerals in its regolith — the loose dust and rock covering its surface. The soil of Earth is a kind of regolith, albeit one loaded with organic content.</li>
 				<li>The cold, thin atmosphere means liquid water currently cannot exist on the Martian surface for any length of time. This means that although this desert planet is just half the diameter of Earth, they have the same amount of dry land.</li>
 				<li>Channels, valleys, and gullies are found all over Mars, and suggest that liquid water might have flowed across the planet's surface in recent times. Some channels can be 60 miles (100 kilometers) wide and 1,200 miles (2,000 kilometers) long. Water may still lie in cracks and pores in underground rock.</li>
 			</ul>
 
-			<h1>Composition & Structure</h1>
+			<h2>Composition & Structure</h2>
 			<ul>
 				<li>Atmospheric composition (by volume)<br />
 					95.32 percent carbon dioxide, 2.7 percent nitrogen, 1.6 percent argon, 0.13 percent oxygen, 0.08 percent carbon monoxide, minor amounts of water, nitrogen oxide, neon, hydrogen-deuterium-oxygen, krypton, xenon</li>
@@ -49,7 +49,7 @@ include 'header.php';
 					Scientists think that on average, the Martian core is about 1,800 and 2,400 miles in diameter (3,000 and 4,000 kilometers), its mantle is about 900 to 1,200 miles (5,400 to 7,200 kilometers) wide and its crust is about 30 miles</li>
 			</ul>
 
-			<h1>Terrain</h1>
+			<h2>Terrain</h2>
 			<p>Mars has 4 very interesting terrestrial sites on its terrain.<br />
 			<ul>
 				<li>Olympus Mons: the largest mountain in the Solar System rising 24 km (78,000 ft.) above the surrounding plain. Its base is more than 500 km in diameter and is rimmed by a cliff 6 km (20,000 ft) high.</li>
