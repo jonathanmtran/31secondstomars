c3192c2457cbbc87cd71178cd672a32b4c66f24b
Jonathan M Tran <jonathanmtran@about.me>
2012-11-27 15:56:07 -0800

    Removed about link from site links

--- footer.php
+++ footer.php
@@ -31,16 +31,15 @@
             <h2>Site Links</h2>
 			<ul class="flist">
 				<li class="top"><a href="index.php">Home</a></li>
-                <li><a href="#">About</a></li>
 				<li><a href="privacy_policy.php">Privacy Policy</a></li>
 			</ul>
 
-            <p>
+			<p>
 			&copy; copyright 2012 31 Seconds to Mars<br />
 			<a href="http://www.bluewebtemplates.com/" title="Website Templates">website templates</a> by <a href="http://www.styleshout.com/">styleshout</a> <br />
 
 			Valid <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> |
-		   	      <a href="http://validator.w3.org/check/referer">XHTML</a>
+					<a href="http://validator.w3.org/check/referer">XHTML</a>
 			</p>
 		</div>
 	
