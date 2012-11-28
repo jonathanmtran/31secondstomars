11359de87cb489e54241694307edd776c46f5e80
Jonathan M Tran <jonathanmtran@about.me>
2012-11-27 16:15:49 -0800

    Needed lowercase accented characters

--- rovers.php
+++ rovers.php
@@ -12,7 +12,7 @@ include 'header.php';
 				<li>Cruise vehicle dimensions: 2.65 meters (8.7 feet) diameter, 1.6 meters (5.2 feet) tall Rover dimensions: 1.5 meter (4.9 feet) high by 2.3 meters (7.5 feet) wide by 1.6 meter (5.2 feet) long</li>
 				<li>Weight: 1,062 kilograms (2,341 pounds) total at launch, consisting of 174-kilogram (384- pound) rover, 365-kilogram (805-pound) lander, 198-kilogram (436-pound) backshell and parachute, 90-kilogram (198-pound) heat shield and 183-kilogram (403-pound) cruise stage, plus 52 kilograms (115 pounds) of propellant</li>
 				<li>Power: Solar panel and lithium-ion battery system providing 140 watts on Mars surface</li>
-				<li>Science instruments: Panoramic cameras, miniature thermal emission spectrometer, M&Ouml;ssbauer spectrometer, alpha particle X-ray spectrometer, microscopic imager, rock abrasion tool, magnet arrays</li>
+				<li>Science instruments: Panoramic cameras, miniature thermal emission spectrometer, M&ouml;ssbauer spectrometer, alpha particle X-ray spectrometer, microscopic imager, rock abrasion tool, magnet arrays</li>
 			</ul>
 
 			<h2>Spirit Mission</h2>
