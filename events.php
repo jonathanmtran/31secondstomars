f3df056aecabfb1033543e5c0d1d7a6b5e9eff32
Jonathan Tran <jtran@cloud.jonathanmtran.com>
2012-11-28 15:40:24 -0800

    Added one actual event and another junk event

--- events.php
+++ events.php
@@ -4,10 +4,20 @@ include 'header.php';
 ?>
 
 		<div id="main">
-		
-			<a name="TemplateInfo"></a>
 			<h1>Events</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut quam et lectus accumsan ultrices quis vitae est. Curabitur ipsum neque, consequat eget dapibus in, vulputate ut risus. Sed non nisl eu magna tincidunt eleifend. Fusce tempus lacinia nibh, ut cursus tortor venenatis nec. Aliquam augue ante, malesuada quis molestie scelerisque, venenatis eget nisi. Curabitur vulputate arcu pharetra magna faucibus id iaculis sapien lobortis. Nunc mattis mollis velit, et aliquet dolor dictum id. Etiam et nulla lectus, sit amet consequat purus. Duis vel erat tellus. Aenean non tortor nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pellentesque semper massa quis dapibus. Fusce consequat porttitor accumsan. Donec augue est, ultricies id laoreet vel, ultricies et libero.</p>
+			
+			<fieldset>
+				<legend>Before December 12, 2012</legend>
+				<p>The Mars Curiosity rover has been nominated as <span style="color: red">TIME</span>'s person of the year 2012. Be sure to vote for Curiosity!<br />
+					<a href="http://www.time.com/time/specials/packages/article/0,28804,2128881_2128882_2129215,00.html">Vote The Mars Rover as TIME's person of the year 2012</a>
+				</p>
+			</fieldset>
+			<br clear="both" />
+			
+			<fieldset>
+				<legend>December 21, 2012</legend>
+				<p>kaboom</p>
+			</fieldset>
 			
 		</div>
 
