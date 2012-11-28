9d79529a8d3be788011ffd1287ad872b4ead6947
Jonathan Tran <jtran@cloud.jonathanmtran.com>
2012-11-28 15:42:19 -0800

    A bit of humour

--- events.php
+++ events.php
@@ -8,7 +8,8 @@ include 'header.php';
 			
 			<fieldset>
 				<legend>Before December 12, 2012</legend>
-				<p>The Mars Curiosity rover has been nominated as <span style="color: red">TIME</span>'s person of the year 2012. Be sure to vote for Curiosity!<br />
+				<h2>Curiosity for Person of the Year 2012</h2>
+				<p>The Mars Curiosity rover has been nominated as <span style="color: red">TIME</span>'s person of the year 2012. Be sure to vote for Curiosity! Voting closes at 11:59pm on December 12, 2012<br />
 					<a href="http://www.time.com/time/specials/packages/article/0,28804,2128881_2128882_2129215,00.html">Vote The Mars Rover as TIME's person of the year 2012</a>
 				</p>
 			</fieldset>
@@ -16,7 +17,8 @@ include 'header.php';
 			
 			<fieldset>
 				<legend>December 21, 2012</legend>
-				<p>kaboom</p>
+				<h2>Martian Takeover?</h2>
+				<p>... or kaboom?</p>
 			</fieldset>
 			
 		</div>
