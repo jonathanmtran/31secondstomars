0f6a10b6260ce8dab28df721c855596d983b67cd
Jonathan M Tran <jonathanmtran@about.me>
2012-11-27 16:11:48 -0800

    Added rovers page

---
+++ rovers.php
@@ -0,0 +1,44 @@
+<?php
+$page = 'information';
+include 'header.php';
+?>
+		<div id="main">
+		
+			<a name="TemplateInfo"></a>
+			<h1>Mars Exploration Rovers</h1>
+
+			<h2>Spacecraft</h2>
+			<ul>
+				<li>Cruise vehicle dimensions: 2.65 meters (8.7 feet) diameter, 1.6 meters (5.2 feet) tall Rover dimensions: 1.5 meter (4.9 feet) high by 2.3 meters (7.5 feet) wide by 1.6 meter (5.2 feet) long</li>
+				<li>Weight: 1,062 kilograms (2,341 pounds) total at launch, consisting of 174-kilogram (384- pound) rover, 365-kilogram (805-pound) lander, 198-kilogram (436-pound) backshell and parachute, 90-kilogram (198-pound) heat shield and 183-kilogram (403-pound) cruise stage, plus 52 kilograms (115 pounds) of propellant</li>
+				<li>Power: Solar panel and lithium-ion battery system providing 140 watts on Mars surface</li>
+				<li>Science instruments: Panoramic cameras, miniature thermal emission spectrometer, Mössbauer spectrometer, alpha particle X-ray spectrometer, microscopic imager, rock abrasion tool, magnet arrays</li>
+			</ul>
+
+			<h2>Spirit Mission</h2>
+			<ul>
+				<li>Launch vehicle: Delta II 7925</li>
+				<li>Launch: June 10, 2003, from Cape Canaveral Air Force Station, Fla.</li>
+				<li>Earth-Mars distance at launch: 103 million kilometers (64 million miles)</li>
+				<li>Mars landing: Jan. 4, 2004, at about 2:30 p.m. local Mars time (signal received at Earth 8:35 p.m. PST Jan. 3)</li>
+				<li>Landing site: Gusev Crater, possible former lake in giant impact crater</li>
+				<li>Earth-Mars distance on landing day: 170.2 million kilometers (105.7 million miles)</li>
+				<li>One-way speed-of-light time Mars-to-Earth on landing day: 9.46 minutes</li>
+				<li>Total distance traveled Earth to Mars (approximate): 487 million kilometers (303 million miles)</li>
+				<li>Near-surface atmospheric temperature at landing site: -100 C (-148 F) to 0 C (32 F)</li>
+				<li>Primary mission: 90 Mars days, or "sols" (equivalent to 92 Earth days)</li>
+			</ul>
+
+			<h2>Opportunity Mission</h2>
+			<ul>
+				<li>Launch vehicle: Delta II 7925H (larger solid-fuel boosters than 7925)</li>
+				<li>Launch: July 7, 2003, from Cape Canaveral Air Force Station, Fla.</li>
+				<li>Earth-Mars distance at launch: 78 million kilometers (48 million miles)</li>
+				<li>Mars landing: Jan. 25, 2004, at about 1:15 p.m. local Mars time (signal received at Earth 9:05 p.m. PST Jan. 24)</li>
+			</ul>
+
+		</div>
+
+<?php
+include 'footer.php';
+?>
