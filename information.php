9265d0df42cdf6eba3adc021a1ea5d9cace36b42
Jonathan Tran <jtran@cloud.jonathanmtran.com>
2012-11-28 16:58:44 -0800

    User experience (hopefully)improvements

--- information.php
+++ information.php
@@ -4,59 +4,70 @@ include 'header.php';
 ?>
 
 		<div id="main">
-		
-			<a name="TemplateInfo"></a>
-			<h1>Information</h1>
-			<h2>Interesting Facts</h2>
+			
+			<h1 id="title">Information</h1>
+			<p style="text-align: center">
+				<a href="#facts">Interesting Facts</a> | <a href="#mdo">Planet mass, diameter and orbit</a> | <a href="#temperature">Temperature</a> | <a href="#exploration">Exploration</a> | <a href="#geography">Geography</a> | <a href="#cs">Composition & Structure</a> | <a href="#terrain">Terrain</a>
+			</p>
+			
+			<h2 id="facts">Interesting Facts</h2>
 			<p>Mars is named after the ancient Roman god of war, as befitting the red planet's bloody color. The Romans copied the ancient Greeks, who named the fourth planet from the sun after their god of war, Ares.</p>
+			<p style="text-align: right"><a href="#title">back to top</a></p>
 			
-			<h2>Planet mass, diameter and orbit</h2>
+			<h2 id="mdo">Planet mass, diameter and orbit</h2>
 			<ul>
-				<li>Orbit:227,940,00 km from the sun</li>
-				<li>Diameter:6,794 km</li>
-				<li>Mass:6.421e23 kg</li>
+				<li>Orbit:227,940,00 km from the sun<br />&nbsp;</li>
+				<li>Diameter:6,794 km<br />&nbsp;</li>
+				<li>Mass:6.421e23 kg<br />&nbsp;</li>
 			</ul>
+			<p style="text-align: right"><a href="#title">back to top</a></p>
 			
-			<h2>Temperature</h2>
+			<h2 id="temperature">Temperature</h2>
 			<ul>
-				<li>Average Temperature -55 C or -67 F</li>
-				<li>Winter: -133 C to -207 F</li>
-				<li>Summer: 27C or 80 F</li>
+				<li>Average Temperature -55 C or -67 F<br />&nbsp;</li>
+				<li>Winter: -133 C to -207 F<br />&nbsp;</li>
+				<li>Summer: 27C or 80 F<br />&nbsp;</li>
 			</ul>
+			<p style="text-align: right"><a href="#title">back to top</a></p>
 			
-			<h2>Exploration</h2>
+			<h2 id="exploration">Exploration</h2>
 			<ul>
-				<li>The first spacecraft to visit Mars was Mariner 4 in 1965.</li>
-				<li>Mars 2 was the first spacecraft to land actually land on mars in 1976.</li>
+				<li>The first spacecraft to visit Mars was Mariner 4 in 1965.<br />&nbsp;</li>
+				<li>Mars 2 was the first spacecraft to land actually land on mars in 1976.<br />&nbsp;</li>
 			</ul>
+			<p style="text-align: right"><a href="#title">back to top</a></p>
 			
-			<h2>Geography</h2>
+			<h2 id="geography">Geography</h2>
 			<ul>
-				<li>The bright rust color Mars is known for is due to to iron-rich minerals in its regolith - the loose dust and rock covering its surface. The soil of Earth is a kind of regolith, albeit one loaded with organic content.</li>
-				<li>The cold, thin atmosphere means liquid water currently cannot exist on the Martian surface for any length of time. This means that although this desert planet is just half the diameter of Earth, they have the same amount of dry land.</li>
-				<li>Channels, valleys, and gullies are found all over Mars, and suggest that liquid water might have flowed across the planet's surface in recent times. Some channels can be 60 miles (100 kilometers) wide and 1,200 miles (2,000 kilometers) long. Water may still lie in cracks and pores in underground rock.</li>
+				<li>The bright rust color Mars is known for is due to to iron-rich minerals in its regolith - the loose dust and rock covering its surface. The soil of Earth is a kind of regolith, albeit one loaded with organic content.<br />&nbsp;</li>
+				<li>The cold, thin atmosphere means liquid water currently cannot exist on the Martian surface for any length of time. This means that although this desert planet is just half the diameter of Earth, they have the same amount of dry land.<br />&nbsp;</li>
+				<li>Channels, valleys, and gullies are found all over Mars, and suggest that liquid water might have flowed across the planet's surface in recent times. Some channels can be 60 miles (100 kilometers) wide and 1,200 miles (2,000 kilometers) long. Water may still lie in cracks and pores in underground rock.<br />&nbsp;</li>
 			</ul>
-
-			<h2>Composition & Structure</h2>
+			<p style="text-align: right"><a href="#title">back to top</a></p>
+			
+			<h2 id="cs">Composition & Structure</h2>
 			<ul>
-				<li>Atmospheric composition (by volume)<br />
-					95.32 percent carbon dioxide, 2.7 percent nitrogen, 1.6 percent argon, 0.13 percent oxygen, 0.08 percent carbon monoxide, minor amounts of water, nitrogen oxide, neon, hydrogen-deuterium-oxygen, krypton, xenon</li>
-				<li>Magnetic Field<br />
-					Mars currently has no global magnetic field, but there are regions of its crust that can be at least 10 times more strongly magnetized than anything measured on Earth, remnants of an ancient global magnetic field.</li>
-				<li>Chemical composition<br />
-					Mars likely has a solid core composed of iron, nickel, and sulfur. The mantle of Mars is probably similar to Earth's in that it is composed mostly of peridotite, which is made up primarily of silicon, oxygen, iron, and magnesium. The crust is probably largely made of the volcanic rock basalt, which is also common in the crusts of the Earth and the moon, although some crustal rocks, especially in the northern hemisphere, may be a form of andesite, a volcanic rock that contains more silica than basalt does.</li>
-				<li>Internal structure<br />
-					Scientists think that on average, the Martian core is about 1,800 and 2,400 miles in diameter (3,000 and 4,000 kilometers), its mantle is about 900 to 1,200 miles (5,400 to 7,200 kilometers) wide and its crust is about 30 miles</li>
+				<li><strong>Atmospheric composition (by volume)</strong><br />
+					95.32 percent carbon dioxide, 2.7 percent nitrogen, 1.6 percent argon, 0.13 percent oxygen, 0.08 percent carbon monoxide, minor amounts of water, nitrogen oxide, neon, hydrogen-deuterium-oxygen, krypton, xenon<br />&nbsp;</li>
+				<li><strong>Magnetic Field</strong><br />
+					Mars currently has no global magnetic field, but there are regions of its crust that can be at least 10 times more strongly magnetized than anything measured on Earth, remnants of an ancient global magnetic field.<br />&nbsp;</li>
+				<li><strong>Chemical composition</strong><br />
+					Mars likely has a solid core composed of iron, nickel, and sulfur. The mantle of Mars is probably similar to Earth's in that it is composed mostly of peridotite, which is made up primarily of silicon, oxygen, iron, and magnesium. The crust is probably largely made of the volcanic rock basalt, which is also common in the crusts of the Earth and the moon, although some crustal rocks, especially in the northern hemisphere, may be a form of andesite, a volcanic rock that contains more silica than basalt does.<br />&nbsp;</li>
+				<li><strong>Internal structure</strong><br />
+					Scientists think that on average, the Martian core is about 1,800 and 2,400 miles in diameter (3,000 and 4,000 kilometers), its mantle is about 900 to 1,200 miles (5,400 to 7,200 kilometers) wide and its crust is about 30 miles<br />&nbsp;</li>
 			</ul>
-
-			<h2>Terrain</h2>
+			<p style="text-align: right"><a href="#title">back to top</a></p>
+			
+			<h2 id="terrain">Terrain</h2>
 			<p>Mars has 4 very interesting terrestrial sites on its terrain.<br />
 			<ul>
-				<li>Olympus Mons: the largest mountain in the Solar System rising 24 km (78,000 ft.) above the surrounding plain. Its base is more than 500 km in diameter and is rimmed by a cliff 6 km (20,000 ft) high.</li>
-				<li>Tharsis: a huge bulge on the Martian surface that is about 4000 km across and 10 km high.</li>
-				<li>Valles Marineris: a system of canyons 4000 km long and from 2 to 7 km deep (top of page);</li>
-				<li>Hellas Planitia: an impact crater in the southern hemisphere over 6 km deep and 2000 km in</li>
+				<li>Olympus Mons: the largest mountain in the Solar System rising 24 km (78,000 ft.) above the surrounding plain. Its base is more than 500 km in diameter and is rimmed by a cliff 6 km (20,000 ft) high.<br />&nbsp;</li>
+				<li>Tharsis: a huge bulge on the Martian surface that is about 4000 km across and 10 km high.<br />&nbsp;</li>
+				<li>Valles Marineris: a system of canyons 4000 km long and from 2 to 7 km deep<br />&nbsp;</li>
+				<li>Hellas Planitia: an impact crater in the southern hemisphere over 6 km deep and 2000 km in<br />&nbsp;</li>
 			</ul>
+			<p style="text-align: right"><a href="#title">back to top</a></p>
+			
 		</div>
 
 <?php
