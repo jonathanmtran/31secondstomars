2108a133c01d12b36ab53d1573f85183f2130511
Jonathan Tran <jtran@cloud.jonathanmtran.com>
2012-11-28 16:52:30 -0800

    Added a quick navigation to the top and added a line break after every bullet for easy reading

--- rovers.php
+++ rovers.php
@@ -3,46 +3,51 @@ $page = 'rovers';
 include 'header.php';
 ?>
 		<div id="main">
-		
-			<a name="TemplateInfo"></a>
-			<h1>Mars Exploration Rovers</h1>
-
-			<h2>Spacecraft</h2>
+			
+			<h1 id="top">Mars Exploration Rovers</h1>
+			<p style="text-align: center">
+				<a href="#spacecraft">Spacecraft</a> | <a href="#curiosity">Curiosity</a> | <a href="#opportunity">Opportunity</a> | <a href="#spirit">Spirit</a>
+			</p>
+			
+			<h2 id="spacecraft">Spacecraft</h2>
 			<ul>
-				<li>Cruise vehicle dimensions: 2.65 meters (8.7 feet) diameter, 1.6 meters (5.2 feet) tall Rover dimensions: 1.5 meter (4.9 feet) high by 2.3 meters (7.5 feet) wide by 1.6 meter (5.2 feet) long</li>
-				<li>Weight: 1,062 kilograms (2,341 pounds) total at launch, consisting of 174-kilogram (384- pound) rover, 365-kilogram (805-pound) lander, 198-kilogram (436-pound) backshell and parachute, 90-kilogram (198-pound) heat shield and 183-kilogram (403-pound) cruise stage, plus 52 kilograms (115 pounds) of propellant</li>
-				<li>Power: Solar panel and lithium-ion battery system providing 140 watts on Mars surface</li>
-				<li>Science instruments: Panoramic cameras, miniature thermal emission spectrometer, M&ouml;ssbauer spectrometer, alpha particle X-ray spectrometer, microscopic imager, rock abrasion tool, magnet arrays</li>
+				<li>Cruise vehicle dimensions: 2.65 meters (8.7 feet) diameter, 1.6 meters (5.2 feet) tall Rover dimensions: 1.5 meter (4.9 feet) high by 2.3 meters (7.5 feet) wide by 1.6 meter (5.2 feet) long<br />&nbsp;</li>
+				<li>Weight: 1,062 kilograms (2,341 pounds) total at launch, consisting of 174-kilogram (384- pound) rover, 365-kilogram (805-pound) lander, 198-kilogram (436-pound) backshell and parachute, 90-kilogram (198-pound) heat shield and 183-kilogram (403-pound) cruise stage, plus 52 kilograms (115 pounds) of propellant<br />&nbsp;</li>
+				<li>Power: Solar panel and lithium-ion battery system providing 140 watts on Mars surface<br />&nbsp;</li>
+				<li>Science instruments: Panoramic cameras, miniature thermal emission spectrometer, M&ouml;ssbauer spectrometer, alpha particle X-ray spectrometer, microscopic imager, rock abrasion tool, magnet arrays<br />&nbsp;</li>
 			</ul>
+			<p style="text-align: right"><a href="#top">back to top</a></p>
 
-			<h2>Curiosity</h2>
+			<h2 id="curiosity">Curiosity</h2>
 			<p><em>Note: We are still compiling information about the latest rover and will update as information is made available</em></p>
 			<ul>
-				<li>Launch vehicle: United Launch Alliance Atlas V</li>
-				<li>Launch: November 26, 2011</li>
+				<li>Launch vehicle: United Launch Alliance Atlas V<br />&nbsp;</li>
+				<li>Launch: November 26, 2011<br />&nbsp;</li>
 			</ul>
 			
-			<h2>Opportunity</h2>
+			<h2 id="opportunity">Opportunity</h2>
 			<ul>
-				<li>Launch vehicle: Delta II 7925H (larger solid-fuel boosters than 7925)</li>
-				<li>Launch: July 7, 2003, from Cape Canaveral Air Force Station, Fla.</li>
-				<li>Earth-Mars distance at launch: 78 million kilometers (48 million miles)</li>
-				<li>Mars landing: Jan. 25, 2004, at about 1:15 p.m. local Mars time (signal received at Earth 9:05 p.m. PST Jan. 24)</li>
+				<li>Launch vehicle: Delta II 7925H (larger solid-fuel boosters than 7925)<br />&nbsp;</li>
+				<li>Launch: July 7, 2003, from Cape Canaveral Air Force Station, Fla.<br />&nbsp;</li>
+				<li>Earth-Mars distance at launch: 78 million kilometers (48 million miles)<br />&nbsp;</li>
+				<li>Mars landing: Jan. 25, 2004, at about 1:15 p.m. local Mars time (signal received at Earth 9:05 p.m. PST Jan. 24)<br />&nbsp;</li>
 			</ul>
+			<p style="text-align: right"><a href="#top">back to top</a></p>
 
-			<h2>Spirit</h2>
+			<h2 id="spirit">Spirit</h2>
 			<ul>
-				<li>Launch vehicle: Delta II 7925</li>
-				<li>Launch: June 10, 2003, from Cape Canaveral Air Force Station, Fla.</li>
-				<li>Earth-Mars distance at launch: 103 million kilometers (64 million miles)</li>
-				<li>Mars landing: Jan. 4, 2004, at about 2:30 p.m. local Mars time (signal received at Earth 8:35 p.m. PST Jan. 3)</li>
-				<li>Landing site: Gusev Crater, possible former lake in giant impact crater</li>
-				<li>Earth-Mars distance on landing day: 170.2 million kilometers (105.7 million miles)</li>
-				<li>One-way speed-of-light time Mars-to-Earth on landing day: 9.46 minutes</li>
-				<li>Total distance traveled Earth to Mars (approximate): 487 million kilometers (303 million miles)</li>
-				<li>Near-surface atmospheric temperature at landing site: -100 C (-148 F) to 0 C (32 F)</li>
-				<li>Primary mission: 90 Mars days, or "sols" (equivalent to 92 Earth days)</li>
+				<li>Launch vehicle: Delta II 7925<br />&nbsp;</li>
+				<li>Launch: June 10, 2003, from Cape Canaveral Air Force Station, Fla.<br />&nbsp;</li>
+				<li>Earth-Mars distance at launch: 103 million kilometers (64 million miles)<br />&nbsp;</li>
+				<li>Mars landing: Jan. 4, 2004, at about 2:30 p.m. local Mars time (signal received at Earth 8:35 p.m. PST Jan. 3)<br />&nbsp;</li>
+				<li>Landing site: Gusev Crater, possible former lake in giant impact crater<br />&nbsp;</li>
+				<li>Earth-Mars distance on landing day: 170.2 million kilometers (105.7 million miles)<br />&nbsp;</li>
+				<li>One-way speed-of-light time Mars-to-Earth on landing day: 9.46 minutes<br />&nbsp;</li>
+				<li>Total distance traveled Earth to Mars (approximate): 487 million kilometers (303 million miles)<br />&nbsp;</li>
+				<li>Near-surface atmospheric temperature at landing site: -100 C (-148 F) to 0 C (32 F)<br />&nbsp;</li>
+				<li>Primary mission: 90 Mars days, or "sols" (equivalent to 92 Earth days)<br />&nbsp;</li>
 			</ul>
+			<p style="text-align: right"><a href="#top">back to top</a></p>
 
 		</div>
 
